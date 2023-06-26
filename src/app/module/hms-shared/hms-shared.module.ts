ed5abad2f972b7374c4857a2b4430cc9dfd8950e
Mofirojean <mofirojean@gmail.com>
2023-06-26 21:53:09 +0100

    chore: added new modules for the UI

--- src/app/module/hms-shared/hms-shared.module.ts
+++ src/app/module/hms-shared/hms-shared.module.ts
@@ -1,17 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ToastModule } from 'primeng/toast';
-
+import { CalendarModule } from 'primeng/calendar';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { DialogModule } from 'primeng/dialog';
+import { InputTextModule } from 'primeng/inputtext';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    ToastModule
+    ToastModule,
+    CalendarModule,
+    InputNumberModule,
+    DialogModule,
+    InputTextModule
   ],
   exports: [
-    ToastModule
+    ToastModule,
+    CalendarModule,
+    InputNumberModule,
+    DialogModule,
+    InputTextModule
   ]
 })
 export class HmsSharedModule { }
